2b8cf95ed5fab8f94088058f72a15fc7bf6b3366
sumel <sumel124@gmail.com>
2017-11-04 10:24:37 -0500

    slot simple child

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,24 @@
 <template lang="pug">
   #app
-    rappi-header
+    child
+      h1(slot="title") Titulo slot
+      div(slot="body")
+        p ddddd ddd
+        p ddddd ddd
 
-    rappi-loader(v-show="isLoading")
-    section.section(v-show="!isLoading")
-      nav.nav
-        .container.has-shadow
-          input.input.is-large(
-            type="text", placeholder="Buscar algo"
-            v-model="searchQuery"
-            )
-          a.button.is-info.is-large(
-            @click="search"
-            ) Buscar
-          a.button.is-danger.is-large &times;
-          p
-            small {{ searchMessage }}
-      .container.results
-        .columns.is-multiline
-          .column.is-one-quarter(v-for="t in tracks")
-            rappi-product(
-              :class="{ 'is-active': t.id === selectedProduct  }",
-              :product="t", @select="setSelectedProduct" )
-
-    rappi-footer
 </template>
 
 <script>
 
-import trackService from '@/services/tracks'
-import RappiFooter from '@/components/layout/Footer.vue'
-import RappiHeader from '@/components/layout/Header.vue'
-import RappiProduct from '@/components/Product.vue'
-import RappiLoader from '@/components/shared/Loader.vue'
+import child from '@/components/ChildComponent.vue'
 
 export default {
   name: 'app',
 
-  components: { RappiFooter, RappiHeader, RappiProduct, RappiLoader },
+  components: { child },
 
   data () {
     return {
-      searchQuery: '',
-      tracks: [],
-      isLoading: false,
-      selectedProduct: ''
-    }
-  },
-
-  computed: {
-    searchMessage () {
-      return `Encontrados: ${this.tracks.length}`
-    }
-  },
-
-  methods: {
-    search () {
-      // console.log(this.searchQuery)
-      // this.tracks = tracks
-      if (!this.searchQuery.length > 0) { return }
-
-      this.isLoading = true
-
-      trackService.search(this.searchQuery)
-        .then(res => {
-          console.log(res)
-          this.tracks = res.tracks.items
-          this.isLoading = false
-        })
-    },
-
-    setSelectedProduct (id) {
-      console.log(id)
-      this.selectedProduct = id
     }
   }
 }
