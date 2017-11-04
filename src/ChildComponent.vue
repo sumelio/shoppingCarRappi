c8fde56717a5033e5d634a9b6a4ffe1ae4de722a
sumel <sumel124@gmail.com>
2017-11-04 07:59:57 -0500

    Local components

---
+++ src/ChildComponent.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+div
+  h1 Este es un componente hijo
+  h2 Hijo dos
+  local-component
+</template>
+
+<script>
+import LocalComponent from './LocalComponent.vue'
+
+export default {
+  components: { LocalComponent }
+}
+</script>
