4308a633696d80e1eae92796bf88344a7b46ceb2
sumel <sumel124@gmail.com>
2017-11-04 00:35:26 -0500

    Copiado de otro lado

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,101 @@
 <template lang="pug">
-  #app
-    section.section
-      nav.nav.has-shadow
-        .container
-          input.input.is-large(
-            type="text", placeholder="Buscar algo"
-            v-model="searchQuery"
-            )
-          a.button.is-info.is-large(
-            @click="search"
-            ) Buscar
-          a.button.is-danger.is-large &times;
-          p
-            small {{ searchMessage }}
-      .container.results
-        .columns
-          .cloumn(v-for="t in tracks") {{ t.name }}
+   #app
+     .container
+      h1.title.is-1.has-text-centered Gestor de Tareas
+      .field.is-3( v-show="!name")
+        .control
+          input#name.input.is-primary(
+          type='text',
+          placeholder='Ingresa tu nombre ',
+          v-model='newName')
+          a.button.is-primary(type="submit", @click="addName") Ok
+      span(v-show="name") Tareas de {{ name }}
+      hr
+      .columns
+        .column
+          h2.title.is-3.has-text-centered Agregar Tarea
+          .columns.is-mobile
+            .column.is-three-quarters
+              .field
+                .control
+                  input.input.is-primary(
+                  type='text',
+                  placeholder='Tarea ',
+                  v-model='newTask.title')
+            .column
+              .field
+                .control
+                  input.input.is-warning(
+                  type='number',
+                  placeholder='0',
+                  v-model='newTask.time')
+              a.button.is-primary(@click="nuevaTarea") +
+        .column
+          h2.title.is-3.has-text-centered Lista de Tareas
+          .block
+            ul
+              li(v-for="( tarea, index) in tasks")
+                span.tag.is-primary {{ tarea.title }} {{ tarea.time }}
+                  button.delete.is-small(@click="removeTask(index)")
+        .column
+          h2.title.is-3.has-text-centered Tiempo requerido
+          .tags.has-addons
+            span.tag.is-medium.is-warning {{ totalHoras }}
+            span.tag.is-primary.is-medium Horas
 </template>
 
 <script>
-const tracks = [
-  { name: 'Muchacha', artist: 'Luis ALbert' },
-  { name: 'Hoy aca en el baile', artist: 'El Pepo' },
-  { name: 'I was made for loving you', artist: 'Kiss' }
-]
 
 export default {
   name: 'app',
   data () {
     return {
-      searchQuery: '',
-      tracks: []
+      newName: null,
+      name: '',
+      tasks: [],
+      newTask: {
+        title: '',
+        time: null
+      }
     }
+  },
+
+  created () {
+    this.name = JSON.parse(window.localStorage.getItem('name'))
+    this.tasks = JSON.parse(window.localStorage.getItem('tasks')) || []
   },
 
   computed: {
-    searchMessage () {
-      return `Encontrados: ${this.tracks.length}`
+    totalHoras () {
+      if (!this.tasks.length) { return 0 }
+
+      let total = 0
+      this.tasks.forEach(tarea => {
+        total += parseInt(tarea.time)
+      })
+
+      return total
     }
   },
-
   methods: {
-    search () {
-      // console.log(this.searchQuery)
-      this.tracks = tracks
+    addName () {
+      this.name = this.newName
+      window.localStorage.setItem('name', JSON.stringify(this.name))
+    },
+    nuevaTarea () {
+      if (this.newTask.title && this.newTask.time) {
+        this.tasks.unshift({
+          title: this.newTask.title,
+          time: this.newTask.time
+        })
+        window.localStorage.setItem('tasks', JSON.stringify(this.tasks))
+        this.newTask.title = ''
+        this.newTask.time = null
+      }
+    },
+    removeTask (index) {
+      this.tasks.splice(index, 1)
+      window.localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
   }
 }
@@ -55,4 +107,11 @@ export default {
   .results {
     margin-top: 50px;
   }
+  #name{
+   box-shadow:none;
+   border-top: none;
+   border-right: none;
+   border-left:none;
+   width:20%;
+ }
 </style>
