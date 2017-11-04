712dca03070e74b25fd22b7a297ea8ac1abb7a97
sumel <sumel124@gmail.com>
2017-11-03 22:08:16 -0500

    Cool movies but without responsive

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,76 @@
 <template lang="pug">
-  #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    #app
+      .header
+        nav
+          .logo
+            img.logo(src='http://s03.s3c.es/imag/_v0/771x421/0/1/5/dos-carretera-peli.jpg')
+          .search-bar
+            input.search(type='text' placeholder='Busca tu peli, actor, actriz ó director favorito')
+          .user
+            div.container
+              p.user-name Juan Luis Rojas
+              i.fa.fa-user-circle-o(aria-hidden='true')
+              i.fa.fa-sort-desc(aria-hidden='true')
+      .content
+        .side-bar
+          .first
+            i.fa.fa-home(aria-hidden='true')
+            p Home
+            i.fa.fa-heart(aria-hidden='true')
+            p Trending
+            i.fa.fa-fire(aria-hidden='true')
+            p More likes
+          .last
+            i.fa.fa-cog(aria-hidden='true')
+            p Settings
+        .content-down
+          h2 Ahora en cine
+          div.container-images
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+          h2 Aclamadas por la crítica
+          div.container-images
+              div.over
+                div.color
+                  h4 The Secret Life of Pets
+                  h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+              div.over
+                div.color
+                  h4 The Secret Life of Pets
+                  h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+              div.over
+                div.color
+                  h4 The Secret Life of Pets
+                  h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+          h2 No te debes  de perder
+          div.container-images
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
+            div.over
+              div.color
+                h4 The Secret Life of Pets
+                h6 The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes.
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Hola Vue'
-    }
-  }
+  name: 'app'
 }
 </script>
 
